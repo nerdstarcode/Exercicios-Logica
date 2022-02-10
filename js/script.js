f18a6037549bf4056d4092d48ff44b40cbe385db
nerdstarcode <sthivendev@gmail.com>
2022-02-09 21:55:20 -0300

    programa F-C

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 //s
 function AreaCirculo(){
-    var raio = parseFloat(document.getElementById("RaioDoCirculo").value);
-    var resposta = 3.14 * (raio * raio);
+    let raio = parseFloat(document.getElementById("RaioDoCirculo").value);
+    let resposta = 3.14 * (raio * raio);
     document.getElementById('RespostaRaio').innerHTML = 'Raio: '+ resposta +'cm';
-    document.getElementById('CirculoVisual').style = 'width: '+ resposta/30+'px; height:'+ resposta/30 +'px;'
+    document.getElementById('CirculoVisual').style = 'width: '+ resposta/(6*raio)+'px; height:'+ resposta/(6*raio) +'px;';
+}
+function FahrenheitToCelcius(){
+    let F = parseFloat(document.getElementById("Fahrenheit").value);
+    let resposta = (F - 32) * (5/9);
+    document.getElementById('Temperatura').innerHTML = 'Fahrenheit: '+ resposta;
+    if (resposta > 0){
+        document.getElementById('Termometro').style = 'background: #000; height:'+ (resposta)/30 +'px;';
+    }else{
+        let negativo = (resposta*resposta)^(1/2);
+        document.getElementById('Termometro').style = 'background: #ff0000; height:'+ (negativo)/(30) +'px;';
+    }
+
 }
